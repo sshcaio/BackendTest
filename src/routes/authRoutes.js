73d48159f1f14b9a4cc8985ba18cddde66c49487
sshcaio <sonyhellish@gmail.com>
2024-03-05 18:55:50 -0300

    feat: register api route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,4 +1,6 @@
 const express = require('express');
 const authRouter = express.Router();
+const { registerController } = require('../msc/controllers/authController');
+authRouter.post('/register', registerController);
 
 module.exports = authRouter;
