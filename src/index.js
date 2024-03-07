d76eb48cfa35730fdf9f884242a832185b381d51
sshcaio <sonyhellish@gmail.com>
2024-03-07 19:59:29 -0300

    fix: env port

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.get('/', (_req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
