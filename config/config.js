e0bbd275e3d3f01a0b979caa6899eeba084b327e
sshcaio <sonyhellish@gmail.com>
2024-03-07 19:20:12 -0300

    feat: vercel postgres

---
+++ config/config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+const options = {
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  host: process.env.POSTGRES_HOST,
+  dialect: "postgres"
+};
+
+module.exports = {
+  development: {
+    ...options,
+  },
+};
