785ab7acea66583e2f5289c340da8abdd2619c5f
sshcaio <sonyhellish@gmail.com>
2024-03-07 21:33:55 -0300

    fix: access control

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const routes = require('./routes');
 const app = express();
 
 app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://example.com"); // Replace with your domain
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use('/auth', routes.authRouter);
 app.use('/products', routes.productRouter);
 
