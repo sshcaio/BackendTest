86c30b725fb25145a64c98027af6d4ae66667954
sshcaio <sonyhellish@gmail.com>
2024-03-07 21:00:19 -0300

    fix:

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const options = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  database: process.env.POSTGRES_DATABASE,
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
   native: true,
