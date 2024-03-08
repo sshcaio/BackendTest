03bd6159c2661bca91db11ea3583006a347ac883
sshcaio <sonyhellish@gmail.com>
2024-03-07 21:21:11 -0300

    fix:

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const options = {
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  host: process.env.POSTGRES_HOST,
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+};
+
+module.exports = {
+  development: {
+    ...options,
+  },
+  test: {
+    ...options,
+  },
+  production: {
+    ...options,
+  },
+};
